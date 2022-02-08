90f4a2ce6039ebcb8732094732b154eed2ab4daf
Gareth Davis <gdavis@softwareinstitute.com>
2022-02-08 09:37:25 +0000

    This is my first commit demoing Git and GitHub to Jan Dynamics

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {        
+
+    static void Main(string[] args)
+
+    {
+        string myName = "Gareth"; //this is where i set the name
+        Console.WriteLine(myName==new string("Gareth"));
+        Console.WriteLine(myName.Equals("Gareth"));
+        myName = "James";
+    }
+
+}
+
